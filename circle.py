e348b72d8efc613a02580114b94997b441cec9f9
Son-pix <solosoni2005@gmail.com>
2025-12-15 11:09:54 +0300

    Add files via upload

--- circle.py
+++ circle.py
@@ -9,3 +9,9 @@ class Circle:
         dx = x - self.center[0]
         dy = y - self.center[1]
         return dx*dx + dy*dy <= self.radius**2
+    @classmethod
+    def from_user_input(cls):
+        cx = float(input("Введите x-координату центра круга: "))
+        cy = float(input("Введите y-координату центра круга: "))
+        assert cx < 0 and cy > 0, "Центр должен быть во 2-й четверти (x < 0, y > 0)" 
+        return cls(cx, cy)
