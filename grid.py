c79f7128b7af42c12a368b9209c44e13b241a27c
Uliana-C <tyk240405@gmail.com>
2025-12-15 17:48:48 +0300

    Add files via upload
    исправления в отрицательной сетке

--- grid.py
+++ grid.py
@@ -1,41 +1,29 @@
 import numpy as np
-from point import Point
-
+from points import Point
 
 class Grid:
-
-    def __init__(self, step: float = 0.01, max_coord: float = 3.0):
+    def init(self, step: float = 0.01,min_coord: float = -3.0, max_coord: float = 3.0):
         self.step = step
         self.max_coord = max_coord
+        self.min_coord = min_coord
         self._build_grid()
 
-
     def _build_grid(self):
-        # Формируем одномерный массив координат и создаем двумерную сетку координат
-        coords = np.arange(self.step, self.max_coord + self.step, self.step)
+        coords = np.arange(self.min_coord, self.max_coord + self.step, self.step)
         self.X1, self.X2 = np.meshgrid(coords, coords)
-        #Преобразуем двумерную сетку в массив
         self.points = np.column_stack((self.X1.ravel(), self.X2.ravel()))
 
     def create_points(self, t0: float = 0.0):
         points = []
-
         for i, coord in enumerate(self.points):
-
             points.append(Point(i, coord, t0))
-
         return points
 
-    # Выбираем точки, лежащие внутри заданной фигуры
     def select_points_inside_shape(self, shape):
-        mask = np.array([
-            shape.contains(x, y) for x, y in self.points
-        ])
-        return self.points[mask]  # True — точка внутри фигуры
+        mask = np.array([shape.contains(x, y) for x, y in self.points])
+        return self.points[mask]
 
-
-    # Определяет строковое представление
-    def __repr__(self):
+    def repr(self):
         return (
             f"Grid(step={self.step}, max_coord={self.max_coord}, "
             f"points={len(self.points)})"
