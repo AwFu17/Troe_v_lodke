890f00c1006852829b1710b641248036f1ac0675
Son-pix <solosoni2005@gmail.com>
2025-12-17 17:22:55 +0300

    Add files via upload

---
+++ wwclassikenum.py
@@ -0,0 +1,16 @@
+# wwclassikenum.py
+from enum import Enum
+from circle import Circle
+from square import Square
+
+class Body(Enum):
+    CIRCLE = 1
+    SQUARE = 2
+
+def create_body(body_type: Body, cx1: float, cx2: float, size: float = 1):
+    if body_type == Body.CIRCLE:
+        return Circle(cx1, cx2, size)
+    elif body_type == Body.SQUARE:
+        return Square(cx1, cx2, size)
+    else:
+        raise ValueError("Неизвестный тип тела")
