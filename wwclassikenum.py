c897caff85b463495970c1187539f6c60642102d
Son-pix <solosoni2005@gmail.com>
2025-12-18 18:07:24 +0300

    Add files via upload

--- wwclassikenum.py
+++ wwclassikenum.py
@@ -5,12 +5,3 @@ from square import Square
 class Body(Enum):
     CIRCLE = 1
     SQUARE = 2
-
-    @classmethod
-    def create_body(cls, body_type, cx1: float, cx2: float, size: float = 1):
-        if body_type == Body.CIRCLE:
-            return Circle(cx1, cx2, size)
-        elif body_type == Body.SQUARE:
-            return Square(cx1, cx2, size)
-        else:
-           raise ValueError("Неизвестный тип тела")
